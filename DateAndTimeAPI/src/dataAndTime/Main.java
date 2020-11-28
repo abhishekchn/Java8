1651dc69e30f6ea8133c979042f9251cea7b0268
vivek <vivek@idea-PC>
2020-11-28 22:40:46 +0530

    DateTimeZone

--- DateAndTimeAPI/src/dataAndTime/Main.java
+++ DateAndTimeAPI/src/dataAndTime/Main.java
@@ -1,26 +1,26 @@
-package dataAndTime;
-
-import java.time.LocalDateTime;
-import java.time.Month;
-
-public class Main {
-
-	public static void main(String[] args) {
-		date date = new date();
-//		date.showDate();
-//		date.showTime();
-//		date.showSeparateDate();
-//		date.showSeparateTime();
-//		date.bothDateAndTime();
-		LocalDateTime ldt = LocalDateTime.of(1995,Month.OCTOBER,02,02,05,10);
-//		System.out.println(ldt);
-//		System.out.println(ldt.getMonthValue());
-//		System.out.println(ldt.getDayOfWeek());
-//		System.out.println(ldt.getDayOfYear());
-//		System.out.printf("After Six months: " + ldt.plusMonths(6));
-		int i = ldt.getDayOfMonth();
-        int j = i+55;
-        System.out.println(j);
-	}
-
-}
+//package dataAndTime;
+//
+//import java.time.LocalDateTime;
+//import java.time.Month;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		date date = new date();
+////		date.showDate();
+////		date.showTime();
+////		date.showSeparateDate();
+////		date.showSeparateTime();
+////		date.bothDateAndTime();
+//		LocalDateTime ldt = LocalDateTime.of(1995,Month.OCTOBER,02,02,05,10);
+////		System.out.println(ldt);
+////		System.out.println(ldt.getMonthValue());
+////		System.out.println(ldt.getDayOfWeek());
+////		System.out.println(ldt.getDayOfYear());
+////		System.out.printf("After Six months: " + ldt.plusMonths(6));
+//		int i = ldt.getDayOfMonth();
+//        int j = i+55;
+//        System.out.println(j);
+//	}
+//
+//}
